4968432453b646fd58bed54afab75f73ed4a136b
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2023-12-14 22:35:02 +0530

    Initial Commit

---
+++ cmd/api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"pex-universe/internal/server"
+	"strconv"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	server := server.New()
+
+	server.RegisterFiberRoutes()
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	err := server.Listen(fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic("cannot start server")
+	}
+}
