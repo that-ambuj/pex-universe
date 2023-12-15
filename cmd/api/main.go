625e691be1a31899afbebe90a1b7827c7b898f87
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2023-12-16 01:23:24 +0530

    feat: setup swagger docs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,10 +9,15 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 )
 
+// @title		Pex Universe API
+// @version	1.0
+// @host		localhost:8080
+// @BasePath	/
 func main() {
 	server := server.New()
 
 	server.RegisterFiberRoutes()
+
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
 	err := server.Listen(fmt.Sprintf(":%d", port))
 	if err != nil {
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,13 +2,31 @@ package server
 
 import (
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/gofiber/swagger"
+	_ "pex-universe/docs"
 )
 
 func (s *FiberServer) RegisterFiberRoutes() {
-	s.App.Get("/", s.HelloWorldHandler)
+	s.App.Get("/swagger/*", swagger.HandlerDefault)
+
+	s.App.Get("/hello", s.HelloWorldHandler)
 	s.App.Get("/health", s.healthHandler)
+
+	s.RegisterAuthRoutes()
 }
 
+type Hello struct {
+	Message string `json:"message"`
+}
+
+// HelloWorldHandler godoc
+//
+//	@Summary	Hello World
+//	@Tags		default
+//	@Produce	json
+//	@Success	200	{object}	Hello
+//	@Router		/hello [get]
 func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
 	resp := map[string]string{
 		"message": "Hello World",
@@ -16,6 +34,13 @@ func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
 	return c.JSON(resp)
 }
 
+// healthHandler godoc
+//
+//	@Summary	Database Health Indicator
+//	@Tags		default
+//	@Produce	json
+//	@Success	200	{object}	Hello
+//	@Router		/health [get]
 func (s *FiberServer) healthHandler(c *fiber.Ctx) error {
 	return c.JSON(s.db.Health())
 }
