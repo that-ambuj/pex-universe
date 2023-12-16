233f0f51521ccf418d676ed4bdb8447d56b0019c
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2023-12-16 15:13:57 +0530

    feat(profile): add routes for profile

---
+++ internal/server/profile.go
@@ -0,0 +1,67 @@
+package server
+
+import (
+	"pex-universe/model"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func (s *FiberServer) RegisterProfileRoutes() {
+	v1 := s.App.Group("/v1")
+
+	v1.Get("/profile", s.profileGet)
+	v1.Put("/profile", s.profilePut)
+}
+
+type ProfileUpdateDto struct {
+	Name string `validate:"required" json:"name" example:"John Doe"`
+}
+
+// profileGet
+//
+//	@Summary	Get Profile Info
+//	@Tags		profile
+//	@Produce	json
+//	@Success	200	{object}	model.User
+//	@Router		/v1/profile [get]
+func (s *FiberServer) profileGet(c *fiber.Ctx) error {
+	user := c.Locals("user").(*model.User)
+
+	return c.JSON(user)
+}
+
+// profilePut
+//
+//	@Summary	Update Profile
+//	@Tags		profile
+//	@Produce	json
+//	@Param		request	body		ProfileUpdateDto	true	"Profile Update Dto"
+//	@Success	201		{object}	model.User
+//	@Router		/v1/profile [put]
+func (s *FiberServer) profilePut(c *fiber.Ctx) error {
+	dto := new(ProfileUpdateDto)
+
+	var err error
+
+	err = c.BodyParser(dto)
+	if err != nil {
+		return fiber.NewError(400, err.Error())
+	}
+
+	err = s.ValidateStruct(dto)
+	if err != nil {
+		return err
+	}
+
+	user := c.Locals("user").(*model.User)
+
+	_, err = s.db.Exec(`UPDATE users SET name = ? WHERE id = ?;`, dto.Name, user.Id)
+	if err != nil {
+		return err
+	}
+
+	newUser := new(model.User)
+	s.db.Get(newUser, `SELECT * FROM users WHERE id = ?;`, user.Id)
+
+	return c.Status(fiber.StatusCreated).JSON(newUser)
+}
