1a12def8af8f6395b45e5f1fbe1af2df976338bd
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2023-12-16 15:14:17 +0530

    test: update handler_test.go

--- tests/handler_test.go
+++ tests/handler_test.go
@@ -16,7 +16,7 @@ func TestHandler(t *testing.T) {
 	// Define a route in the Fiber app
 	app.Get("/", s.HelloWorldHandler)
 	// Create a test HTTP request
-	req, err := http.NewRequest("GET", "/", nil)
+	req, err := http.NewRequest("GET", "/hello", nil)
 	if err != nil {
 		t.Fatalf("error creating request. Err: %v", err)
 	}
