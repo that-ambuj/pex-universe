1d07d4c73261dc4a0fd9efcddcbc91432033d411
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2023-12-19 23:42:00 +0530

    fix: add new NullString type to fix sql.NullString

---
+++ types/string.go
@@ -0,0 +1,17 @@
+package types
+
+import (
+	"database/sql"
+	"encoding/json"
+)
+
+type NullString struct {
+	sql.NullString
+}
+
+func (ns *NullString) MarshalJSON() ([]byte, error) {
+	if !ns.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(ns.String)
+}
