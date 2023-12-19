05be4814c6f9523bf44701fd4a7905c553211476
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2023-12-20 00:12:07 +0530

    feat(profile): add addresses section on user profile

--- internal/server/profile.go
+++ internal/server/profile.go
@@ -27,6 +27,84 @@ type ProfileUpdateDto struct {
 func (s *FiberServer) profileGet(c *fiber.Ctx) error {
 	user := c.Locals("user").(*model.User)
 
+	user.Addresses = []*model.Address{}
+
+	query := `
+SELECT a.id,
+	a.created_at,
+	a.updated_at,
+	a.verified,
+	a.first_name,
+	a.last_name,
+	a.company,
+	a.street_address,
+	a.street_address2,
+	a.city,
+	a.zip,
+	a.phone,
+	a.ext,
+	a.email,
+
+	s.name,
+	s.full_name,
+	s.tax,
+	s.info,
+
+	c.name,
+	c.display_name,
+	c.pp_code,
+	c.locked
+FROM addresses a
+         JOIN states s ON a.state_id = s.id
+         JOIN countries c ON a.country_id = c.id
+WHERE a.user_id = ?;
+`
+
+	rows, err := s.db.Queryx(query, user.Id)
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		addr := &model.Address{}
+
+		addr.State = &model.State{}
+		addr.Country = &model.Country{}
+
+		err = rows.Scan(
+			&addr.Id,
+			&addr.CreatedAt,
+			&addr.UpdatedAt,
+			&addr.Verified,
+			&addr.FirstName,
+			&addr.LastName,
+			&addr.Company,
+			&addr.StreetAddress1,
+			&addr.StreetAddress2,
+			&addr.City,
+			&addr.Zip,
+			&addr.Phone,
+			&addr.Ext,
+			&addr.Email,
+
+			&addr.State.Name,
+			&addr.State.FullName,
+			&addr.State.Tax,
+			&addr.State.Info,
+
+			&addr.Country.Id,
+			&addr.Country.Name,
+			&addr.Country.DisplayName,
+			&addr.Country.PpCode,
+		)
+
+		if err != nil {
+			return err
+		}
+
+		user.Addresses = append(user.Addresses, addr)
+	}
+
 	return c.JSON(user)
 }
 
