c0384a29adc8caa8f28685a4b6e093d3afe7df04
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2023-12-24 22:01:27 +0530

    model: add PaginationDto

--- model/common.go
+++ model/common.go
@@ -7,3 +7,18 @@ type AutoInc struct {
 	CreatedAt *time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt *time.Time `db:"updated_at" json:"updated_at"`
 }
+
+type PaginationDto struct {
+	Page  int
+	Limit int
+}
+
+func (p *PaginationDto) Skip() int {
+	return (p.Page - 1) * p.Limit
+}
+
+type PaginatedResponse struct {
+	Data        interface{}
+	CurrentPage int
+	TotalPages  int
+}
