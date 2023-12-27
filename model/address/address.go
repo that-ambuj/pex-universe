cb658722aa03fa58b6aeea158e9181ffc3c80ad1
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2023-12-28 01:11:34 +0530

    model: add examples and validation to address dtos

--- model/address/address.go
+++ model/address/address.go
@@ -10,16 +10,16 @@ type (
 		CreatedAt      time.Time
 		UpdatedAt      time.Time
 		Verified       bool
-		FirstName      string
-		LastName       string
+		FirstName      string `example:"John"`
+		LastName       string `example:"Doe"`
 		Company        *string
-		StreetAddress1 string `db:"street_address" gorm:"column:street_address"`
+		StreetAddress1 string `gorm:"column:street_address"`
 		StreetAddress2 *string
-		City           string
+		City           string `example:"Tokyo"`
 		Zip            string
-		Phone          string
+		Phone          string `example:"+11349503120"`
 		Ext            string
-		Email          string
+		Email          string `example:"john@example.com"`
 
 		UserID uint `json:"-"`
 
@@ -54,34 +54,34 @@ type (
 
 type (
 	AddressCreateDto struct {
-		FirstName      string
-		LastName       string
+		FirstName      string `example:"John"`
+		LastName       string `example:"Doe"`
 		Company        *string
-		StreetAddress1 string `db:"street_address" gorm:"column:street_address"`
+		StreetAddress1 string `gorm:"column:street_address"`
 		StreetAddress2 *string
-		City           string
-		Zip            string
-		Phone          string
+		City           string `example:"Los Angeles"`
+		Zip            string `validate:"numeric"`
+		Phone          string `validate:"e164" example:"+12380941034"`
 		Ext            string
-		Email          string
+		Email          string `validate:"email" example:"john@example.com"`
 
 		UserID    uint `json:"-"`
-		StateID   uint
-		CountryID uint
+		StateID   uint `validate:"min=1"`
+		CountryID uint `validate:"min=1"`
 	}
 
 	AddressUpdateDto struct {
-		FirstName      *string
-		LastName       *string
+		FirstName      *string `example:"Jane"`
+		LastName       *string `example:"Doe"`
 		Company        *string
-		StreetAddress1 *string `db:"street_address" gorm:"column:street_address"`
+		StreetAddress1 *string `gorm:"column:street_address"`
 		StreetAddress2 *string
-		City           *string
-		Zip            *string
-		Phone          *string
+		City           *string `example:"New York"`
+		Zip            *string `validate:"omitempty,numeric"`
+		Phone          *string `validate:"omitempty,e164" example:"+12380941034"`
 		Ext            *string
-		Email          *string
-		StateId        uint64
-		CountryId      uint64
+		Email          *string `validate:"omitempty,email" example:"john@example.com"`
+		StateId        uint64  `validate:"omitempty,min=1"`
+		CountryId      uint64  `validate:"omitempty,min=1"`
 	}
 )
