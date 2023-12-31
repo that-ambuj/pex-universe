d03926e8f56315bcc481af82cd7cd55987ad85b2
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2024-01-01 00:29:07 +0530

    feat(model/category): add AfterFind hook to populate image path

--- model/product/category.go
+++ model/product/category.go
@@ -1,6 +1,12 @@
 package product
 
-import "time"
+import (
+	"log"
+	"net/url"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Category struct {
 	ID                      uint `json:"id"`
@@ -27,5 +33,24 @@ type Category struct {
 	Published               bool
 	CreatedAt               *time.Time
 	UpdatedAt               *time.Time
-	Children                []*Category `gorm:"many2many:category_children"`
+	Children                []Category `gorm:"many2many:category_children"`
+}
+
+var categoryImagePrefix = "https://pexuniverse.com/uploads/categories/"
+
+func (c *Category) AfterFind(tx *gorm.DB) error {
+
+	if c.Image == nil || *c.Image == "" {
+		return nil
+	}
+
+	u, err := url.Parse(categoryImagePrefix + *c.Image)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	*c.Image = u.String()
+
+	return nil
 }
