4b41f78d874a12c13739c99996063dd04cbdebf5
Ambuj Kumar <97109381+that-ambuj@users.noreply.github.com>
2024-01-19 21:48:16 +0530

    feat: add model Cart

---
+++ model/user/cart.go
@@ -0,0 +1,49 @@
+package user
+
+import (
+	"time"
+)
+
+type Cart struct {
+	ID        uint  `json:"id"`
+	UserID    *uint `json:"-"`
+	CreatedAt *time.Time
+	UpdatedAt *time.Time
+	Items     []CartProduct
+}
+
+func (Cart) TableName() string {
+	return "cart"
+}
+
+type CartProduct struct {
+	ID            uint `json:"id"`
+	CartID        uint `json:"-"`
+	ProductID     int  `json:"product_id"`
+	Title         string
+	Manufacturer  string
+	PartNumber    string
+	Qty           int
+	MqtyID        *int `json:"mqty_id"` // What are these three?
+	Mqty          *int
+	MqtyLabel     *string
+	Price         float32
+	StartingPrice float32 // What is this?
+	Weight        float32
+	WeightUnits   string
+	SavedForLater bool
+	CreatedAt     *time.Time
+	UpdatedAt     *time.Time
+}
+
+// TODO: Know and use other fields of CartProduct
+type CartProductCreateDto struct {
+	ProductID     int  `validate:"required" json:"product_id"`
+	Qty           int  `validate:"required"`
+	SavedForLater bool `validate:"omitempty"`
+}
+
+type CartProductUpdateDto struct {
+	Qty           int  `validate:"omitempty"`
+	SavedForLater bool `validate:"omitempty"`
+}
